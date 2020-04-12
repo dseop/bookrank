f797119c97a67b6630ebb6d9088dc68ab1caf3a1
dseop <ehdtjq4650@gmail.com>
2020-04-12 17:57:38 +0900

    today busy

--- newgit.py
+++ newgit.py
@@ -1,31 +1,45 @@
-import bookrank.crawling as cr
+from pandas import DataFrame as df
+import crawling as cr
 
 url = 'http://www.yes24.com/24/Category/BestSeller?CategoryNumber=001&sumgb=06' # yes24 종합베스트셀러 / 20개씩 / 최근 7일 간
 par_html = cr.makepar(url) # parsed html
 
-book = [] # book name or code < yes24 경우 도서별 code 얻을 수 있음
-def bookrank(par_html) :
-    par_html.find('tbody')
+book_date_list = []
 
+t_list = []
+sp_list = []
 
-print('hello')
+tmp = par_html.find_all('td', 'goodsTxtInfo')[0]
+tmp.a.get('href')
+link_url = 'yes24.com/Product/Goods/'
+move_url = link_url+t_list[0][1]
 
-##일단 기본적인 것들을 코딩해보자
+for tmp in par_html.find_all('td', 'goodsTxtInfo'):
+    t_list.append((tmp.a.text, tmp.a.get('href').split('/')[3])) # tuple version
 
-a=1
-b=2
+    tmp_url = 'http://www.yes24.com'+tmp.a.get('href')
+    
 
-print(a+b)
+#   # t_list.append(tmp.a.text)
+    # #insert book title
+    # href_list.append(tmp.a.get('href'))
+    # code_list.append(tmp.a.get('href').split('/')[3])
+    #    
 
-html = "www.yes24.co.kr"
-print(html)
+# a_list = []
+# pu_list = []
+aupu_list = []
+d_list = []
 
-url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=07&FetchSize=80" 
-# ex) &year=2020&month=4&day=8 이것도 추가 가능
-pagenumber = "&PageNumber=%d" %1
+# tmp = par_html.findAll('div','aupu')[0]
+# tmp2 = list(tmp.stripped_strings)
+# tmp2[len(tmp2)-1][2:] # 0000년 00월만 빼내는 코드
 
-url_2 = url + pagenumber
-print(url_2)
-
-
+for tmp in par_html.find_all('div', 'aupu'):
+    tmp_list = []
+    for a_s in tmp.find_all('a'):
+        tmp_list.append(a_s.text)
+    aupu_list.append(tmp_list) # aupu_list[last] == publisher
+    tmp2 = list(tmp.stripped_strings)
+    d_list.append(tmp2[len(tmp2)-1][2:])
 
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print(1+1)
+#how can i use preview?
+
